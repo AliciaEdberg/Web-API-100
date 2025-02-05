836c8be6e31c38cf1b4b489e4750cedf3fcd02ec
Alicia Edberg <A57aliciav@yahoo.com>
2025-02-05 11:09:00 -0500

    check in changes to unit testing

--- src/SoftwareCatalogSolution/SoftwareCatalog.Api/Catalog/CatalogController.cs
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Api/Catalog/CatalogController.cs
@@ -64,6 +64,35 @@ public class CatalogController(IDocumentSession session) : ControllerBase
         };
         return StatusCode(201, fakeResponse);
     }
+    [HttpPost("/vendors/microsoft/paid")]
+    public async Task<ActionResult> AddPaidItemToCatalogAsync(
+       [FromBody] CatalogItemRequestModel request)
+    {
+
+        // TODO: 1. Do Some validation
+        // TODO : 2 save it to the database.
+        var entityToSave = new CatalogItemEntity
+        {
+            Id = Guid.NewGuid(),
+            Licence = CatalogItemLicenceTypes.Paid,
+            Name = request.Name,
+            Vendor = "Microsoft"
+        };
+
+        session.Store(entityToSave);
+        await session.SaveChangesAsync(); // Do the actual work and save into the database
+
+        // Step 3: send them the response
+        // fake this for right now
+        var fakeResponse = new CatalogItemResponseDetailsModel
+        {
+            Id = entityToSave.Id,
+            Licence = entityToSave.Licence,
+            Name = entityToSave.Name,
+            Vendor = entityToSave.Vendor
+        };
+        return StatusCode(201, fakeResponse);
+    }
 }
 
 
